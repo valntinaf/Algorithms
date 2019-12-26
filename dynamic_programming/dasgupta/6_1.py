e34e2a1982ed4f7f50114a8dcab3489a6fedde7e
valffe <feruerevalentina@gmail.com>
2019-12-26 15:36:09 -0500

    Memory array working

--- dynamic_programming/dasgupta/6_1.py
+++ dynamic_programming/dasgupta/6_1.py
@@ -5,17 +5,18 @@ def continuous_subsequence(s):
     for i in range(len(s)):
         m_i = i+1
         if s[i] < 0:
-            mem[m_i][0] = max(mem[i][0]+s[i],s[i])
+            if mem[i][0] != 0:
+                mem[m_i][0] = max(mem[i][0]+s[i],s[i])
         else:
             mem[m_i][0] = mem[i][0]+s[i]
 
         mem[m_i][1] = max(mem[i][1],mem[m_i][0])
+        print(mem)
 
     print("Memory:")
-    print(mem)
     print("Solution:")
     print(result)
     return 0;
 
-a = [-1,52,1,9,48,8]
+a = [-1,52,-10,9,48,8]
 continuous_subsequence(a)
