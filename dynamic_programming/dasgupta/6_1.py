1485a9505f341c6f1a92285d921901d002df06ae
valffe <feruerevalentina@gmail.com>
2019-12-26 15:29:05 -0500

    first started

---
+++ dynamic_programming/dasgupta/6_1.py
@@ -0,0 +1,21 @@
+def continuous_subsequence(s):
+    mem = [[0 for i in range(2)] for j in range(len(s)+1)]
+    result = []
+    sum = 0
+    for i in range(len(s)):
+        m_i = i+1
+        if s[i] < 0:
+            mem[m_i][0] = max(mem[i][0]+s[i],s[i])
+        else:
+            mem[m_i][0] = mem[i][0]+s[i]
+
+        mem[m_i][1] = max(mem[i][1],mem[m_i][0])
+
+    print("Memory:")
+    print(mem)
+    print("Solution:")
+    print(result)
+    return 0;
+
+a = [-1,52,1,9,48,8]
+continuous_subsequence(a)
